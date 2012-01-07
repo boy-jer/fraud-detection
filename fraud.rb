883a6dc4582a5f2e7a16b104977119ccb2eba0fd
farrspace <brian.farr@gmail.com>
2012-01-07 02:57:07 -0500

    tested

--- fraud.rb
+++ fraud.rb
@@ -1,27 +1,49 @@
+# My basic thinking on this:
+# Within each deal, digest each flattened address and see if its a repeat,
+# the same with email addresses.
+# 
+# Not worried about collisions, because we still check the matches.
+# 
+# Oh also I'm re-learning ruby at the same time I'm doing this coding challenge.  
+# Trying to call two birds with one stone.  Yeah...
+
+
 require 'zlib'
 
+$deals = Array.new
+$fraud_list = Array.new
+
+#flatten variations of email addresses into the same thing
+# eg bugs.BUNNY+groupon@gmail.com -> bugsbunny@gmail.com
 def flatten_email(email)
 	#split account part
 	at_pos = email.index('@')	
 	e = email.slice(0, at_pos)
 	
 	#remove +postfix
-	plus_pos = e.index('+')
-	e.slice!(0, plus_pos)
+	if plus_pos = e.index('+')				
+		e.slice!(0, plus_pos)
+	end
 		
 	#remove periods from account part
 	e.delete!('.')
 	
+	#rejoin with @domain	
+	e = e + email.slice(at_pos, email.length)
+	
 	#case insensitive
 	e.downcase!	
-	
+		
 	#return
 	e	
 end
 
+#flatten street variations into the same thing
+# eg StReEt -> st
+# eg ST -> st
 def flatten_street(street)
 	#case insensitive
-	s.downcase(street_address)
+	s = street.downcase
 
 	#road abbreviation insensitive
 	s.sub!("road", 'rd')
@@ -31,9 +53,12 @@ def flatten_street(street)
 	s	
 end
 
+#flatten state variations into the same thing
+#eg cALIFORNIA -> ca
+#eg IL -> il
 def flatten_state(state)
 	#case insensitive, state abbreviation insensitive	
-	s.downcase(state)
+	s = state.downcase
 	
 	#state abbreviation insensitive
 	s.sub!("illinois", 'il')
@@ -44,6 +69,11 @@ def flatten_state(state)
 	s	
 end
 
+#Determine  if two records constitute fraudulent orders
+# They either have:
+# 	the same deal, same email, and differing credit card
+# or:
+# 	the same deal, same address, and differing credit card
 def records_fraudulent?(a, b)
 	result = false
 	
@@ -51,20 +81,27 @@ def records_fraudulent?(a, b)
 		if a.email == b.email
 			if a.cc != b.cc
 				result = true
+			end
+		end	
 		if a.address == b.address
 			if a.cc != b.cc
 				result = true
-
+			end
+		end
+	end
+	
 	result
 end
 
 class Deal
-	@records = {}
-	@haddresses = {}
-	@hemails = {}
+	attr_accessor :deal, :records, :haddresses, :hemails
+
+	def initialize(deal)
+		@records = Hash.new			# key: order id. value: record
+		@haddresses = Hash.new 	# key: crc of address and deal. value: order id
+		@hemails = Hash.new 		# key: crc of email and deal. value: order id
 	
-	def new(deal)
-		self.deal = deal
+		@deal = deal						# deal id
 	end
 	
 	def add_record(record, address_crc, email_crc)
@@ -73,69 +110,75 @@ class Deal
 		#add crc to address hash map or check fraud
 		if matched_order = @haddresses.fetch(address_crc, false)
 			#collision, check for fraud
-			if record_fraudulent?( @records[matched_order], record)	
-				fraudulent << matched_order << record.order
+			if records_fraudulent?( @records[matched_order], record)	
+				$fraud_list << matched_order << record.order
 			end
 		else	
-			@haddresses[address_crc] = record[0]
+			@haddresses[address_crc] = record.order
 		end
 		
 		#add crc to email hash map or check fraud
 		if matched_order = @hemails.fetch(email_crc, false)
 			#collision, check for fraud			
-			if record_fraudulent?( @records[matched_order], record)
-				fraudulent << matched_order << record.order
+			if records_fraudulent?( @records[matched_order], record)
+				$fraud_list << matched_order << record.order
 			end
 		else	
-			@hemails[email_crc] = record[0]
+			@hemails[email_crc] = record.order
 		end
 		
 	end
 end
 
 class Record
-	def new(order, deal, email, address, cc)
-		self.order = order
-		self.deal = deal
-		self.email = email
-		self.address = address
-		self.cc = cc	
+	attr_accessor :order, :deal, :email, :address, :cc
+
+	def initialize(order, deal, email, address, cc)
+		@order = order
+		@deal = deal
+		@email = email
+		@address = address
+		@cc = cc	
 	end
 end
 
-count = STDIN.gets
+def check
+	count = STDIN.gets
 
-deals = []
-fraud_list = []
-
-while line = STDIN.gets
-	line.split!(',')
-	
-	order		= line[0]
-	deal	 	= line[1]
-	email	 	= line[2]
-	street 	= line[3]
-	city 		= line[4]
-	state		= line[5]
-	zip			= line[6]
-	cc			= line[7]
-	
-	email = flatten_email(email)	
-	street = flatten_street(street)
-	city = city.downcase!
-	state = flatten_state(state)	
+	#repeatedly get new record lines
+	while line = STDIN.gets
+		line = line.chomp.split(',')
 		
-	record = Record.new(order, deal, email, street+city+state+zip, cc)
-	
-	address_crc = Zlib::crc32( street + city + state + zip + deal )	
-	email_crc = Zlib::crc32( email + deal )
-	
-	if !deals.fetch(order, false)
-		deals[order] = Deal.new(order)		
+		order		= line[0]
+		deal	 	= line[1]
+		email	 	= line[2]
+		street 	= line[3]
+		city 		= line[4]
+		state		= line[5]
+		zip			= line[6]
+		cc			= line[7]
+		
+		order = order.to_i
+		deal = deal.to_i
+		
+		email = flatten_email(email)	
+		street = flatten_street(street)
+		city = city.downcase!
+		state = flatten_state(state)	
+			
+		record = Record.new(order, deal, email, street+city+state+zip, cc)
+		
+		address_crc = Zlib::crc32( street + city + state + zip )	
+		email_crc = Zlib::crc32( email )
+		
+		if $deals.fetch(deal, nil).nil?
+			$deals[deal] = Deal.new(order)		
+		end
+		
+		$deals[deal].add_record(record, address_crc, email_crc)	
 	end
 	
-	deals[order].add_record(record, address_crc, email_crc)
-	
-	puts fraud_list.join(",")
+	puts $fraud_list.join(',')
 end
 
+check
